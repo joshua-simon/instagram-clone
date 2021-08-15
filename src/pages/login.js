85ffea724d45180dc0b86192df307cbc9e3720ab
Joshua-Simon <joshy.d.simon@gmail.com>
2021-08-15 23:13:59 +1200

    login page stuff done

--- src/pages/login.js
+++ src/pages/login.js
@@ -23,12 +23,32 @@ const Login = () => {
         document.title = 'Login - Instagram'
     })
 
-
     return (
-        <div>
-            login page
+        <div className = "container flex mx-auto max-w-screen-md items-center h-screen">
+            <div className = "flex w-3/5">
+                <img src = '/images/iphone-with-profile.jpg'/>
+            </div>
+            <div className = "flex flex-col w-2/5">
+                <h1 className = "flex justify-center w-full">
+                    <img src = "/images/logo.png"/>
+                </h1>
+                {error && <p className ="mb-4 text-xs text-red-primary">{error}</p>}
+
+            <form onSubmit = {handleLogin}>
+                <input
+                    aria-label = "Enter your email address"
+                    type = "text"
+                    placeholder = "Email address"
+                    className = "text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+                />
+            </form>  
+            </div>
         </div>
     )
 }
 
 export default Login
+
+
+// text-red-primary
+//text-gray-base
