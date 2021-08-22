cbfb520ed13c978b74f579eb88a6af770da7dd1f
Joshua-Simon <joshy.d.simon@gmail.com>
2021-08-22 21:50:06 +1200

    more login page stuff done

--- src/pages/login.js
+++ src/pages/login.js
@@ -40,8 +40,27 @@ const Login = () => {
                     type = "text"
                     placeholder = "Email address"
                     className = "text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+                    onChange = { ({ target }) => setEmail(target.value)}
                 />
+                <input
+                    aria-label = "Enter your password"
+                    type = "password"
+                    placeholder = "Password"
+                    className = "text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+                    onChange = { ({ target }) => setPassword(target.value)}
+                />
+                <button
+                disabled = {isInvalid}
+                type = "submit"
+                className = {`bg-blue-500 text-white w-full rounded h-8 font-bold ${isInvalid && ' opacity-50'}`}>
+                Log In
+                </button>
             </form>  
+            </div>
+            <div className = "flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+                <p className = 'text-sm'>
+                    Don't have an account? {``}
+                </p>
             </div>
         </div>
     )
