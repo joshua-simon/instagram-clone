d0d6f60b784aadce685a14775ef0d095e1d5e953
Joshua-Simon <joshy.d.simon@gmail.com>
2021-09-20 17:35:57 +1200

    sign up page started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import * as ROUTES from './constants/routes'
 import Login from './pages/login'
+import SignUp from './pages/signup'
 
 function App() {
   return (
     <Router>
         <Switch>
-          <Route path = '/login' component = {Login} />
+          <Route path = {ROUTES.LOGIN} component = {Login} />
+          <Route path = {ROUTES.SIGN_UP} component = {SignUp} />
         </Switch>
     </Router>
   )
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,31 +4,30 @@ import { useState, useContext, useEffect } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import * as ROUTES from '../constants/routes'
 
-const Login = () => {
+const SignUp = () => {
 
     const history = useHistory()
     const { firebase } = useContext(FirebaseContext)
 
-    const [ email, setEmail ]  = useState('')
+    const [ username, setUsername]  = useState('')
+    const [ fullName, setFullName]  = useState('')
+    const [email, setEmailAddress] = useState('');
     const [ password, setPassword ]  = useState('')
     const [ error, setError ]  = useState('')
 
     const isInvalid = password === '' || email === ''
 
-    const handleLogin = async (e) => {
+    const handleSignUp = async (e) => {
         e.preventDefault()
         try {
-            await firebase.auth().signInWithEmailAndPassword(email, password)
-            history.push(ROUTES.DASHBOARD)
+
         } catch(error) {
-            setEmail('');
-            setPassword('');
-            setError(error.message)
+
         }
     }
 
     useEffect(() => {
-        document.title = 'Login - Instagram'
+        document.title = 'Sign Up - Instagram'
     })
 
     return (
@@ -42,20 +41,36 @@ const Login = () => {
                 </h1>
                 {error && <p className ="mb-4 text-xs text-red-primary">{error}</p>}
 
-            <form onSubmit = {handleLogin}>
+            <form onSubmit = {handleSignUp}>
                 <input
-                    aria-label = "Enter your email address"
+                    aria-label = "Enter your username"
                     type = "text"
-                    placeholder = "Email address"
+                    placeholder = "Username"
                     className = "text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
-                    onChange = { ({ target }) => setEmail(target.value)}
+                    onChange = { ({ target }) => setUsername(target.value)}
                 />
                 <input
-                    aria-label = "Enter your password"
+                    aria-label = "Enter your full name"
                     type = "password"
-                    placeholder = "Password"
+                    placeholder = "Full name"
                     className = "text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
-                    onChange = { ({ target }) => setPassword(target.value)}
+                    onChange = { ({ target }) => setFullName(target.value)}
+                />
+                <input
+                aria-label="Enter your email address"
+                type="text"
+                placeholder="Email address"
+                className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                onChange={({ target }) => setEmailAddress(target.value)}
+                value={email}
+                />
+                <input
+                aria-label="Enter your password"
+                type="password"
+                placeholder="Password"
+                className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                onChange={({ target }) => setPassword(target.value)}
+                value={password}
                 />
                 <button
                 disabled = {isInvalid}
@@ -67,8 +82,8 @@ const Login = () => {
             </div>
             <div className = "flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
                 <p className = 'text-sm'>
-                    Don't have an account? {``}
-                    <Link to = {ROUTES.SIGN_UP} className = "font-bold text-blue-medium">
+                    Have an account? {``}
+                    <Link to = "/signup" className = "font-bold text-blue-medium">
                         Sign up
                     </Link>
                 </p>
@@ -77,5 +92,4 @@ const Login = () => {
     )
 }
 
-export default Login
-
+export default SignUp
