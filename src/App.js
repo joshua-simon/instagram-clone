f63e9e7d6901c40e685b3f18ddb7fe2dce49b633
Joshua Simon <joshy.d.simon@gmail.com>
2021-10-03 22:28:20 +1300

    auth listener created

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import Login from './pages/login'
 import SignUp from './pages/signup'
 import NotFound from './pages/not-found'
 import Dashboard from './pages/dashboard'
+import useAuthListener from './hooks/use-auth-listener'
+import UserContext from './context/user'
 
 function App() {
+  const { user } = useAuthListener()
   return (
+  <UserContext.Provider value = {{user}}>
     <Router>
         <Switch>
           <Route path = {ROUTES.LOGIN} component = {Login} />
@@ -16,6 +20,7 @@ function App() {
           <Route  component = {NotFound} />
         </Switch>
     </Router>
+  </UserContext.Provider>
   )
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-
+import React from 'react'
 
 const Header = () => {
     return(
@@ -9,3 +9,4 @@ const Header = () => {
 }
 
 export default Header
+
